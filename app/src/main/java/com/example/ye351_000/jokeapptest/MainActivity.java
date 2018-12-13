a00ab4a18d3fb8b7e3a5f7880dad849fb8ec8135
Evan Yacek <ye351190@yahoo.com>
2018-12-13 00:41:35 -0600

    imported ads into main activity

--- app/src/main/java/com/example/ye351_000/jokeapptest/MainActivity.java
+++ app/src/main/java/com/example/ye351_000/jokeapptest/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.ye351_000.jokeapptest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.google.android.gms.ads.MobileAds;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        MobileAds.initialize(this, "ca-app-pub-8913329212443162~478743485");
     }
 }
