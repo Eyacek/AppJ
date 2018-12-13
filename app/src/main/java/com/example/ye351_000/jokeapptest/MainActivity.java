4df150583f64fefa1b1ba0b3ab8a734ecb292e40
Evan Yacek <ye351190@yahoo.com>
2018-12-13 00:46:17 -0600

    Added banner ad congif

--- app/src/main/java/com/example/ye351_000/jokeapptest/MainActivity.java
+++ app/src/main/java/com/example/ye351_000/jokeapptest/MainActivity.java
@@ -3,13 +3,19 @@ package com.example.ye351_000.jokeapptest;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private AdView mAdView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         MobileAds.initialize(this, "ca-app-pub-8913329212443162~478743485");
+
+        mAdView = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
     }
 }
